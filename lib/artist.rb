5ee48286fa5ff8b34d91c7aa04a38ebbf1c9bce0
Tom White <eugene.white@gmail.com>
2019-06-15 16:47:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 class Artist
+  extend Memorable, Findable
+
   attr_accessor :name
   attr_reader :songs
 
@@ -18,14 +20,6 @@ class Artist
   def self.all
     @@artists
   end
-
- # def self.reset_all
-  #  self.all.clear
- # end
-
- # def self.count
-  #  self.all.count
-  #end
 
   def add_song(song)
     @songs << song
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,9 @@
 require 'pry'
 
 class Song
+
+  extend Memorable
+
   attr_accessor :name
   attr_reader :artist
 
@@ -17,14 +20,6 @@ class Song
   def self.all
     @@songs
   end
-
-  #def self.reset_all
-  #  self.all.clear
-  #end
-
-  #def self.count
-    #self.all.count
-  #end
 
   def artist=(artist)
     @artist = artist
