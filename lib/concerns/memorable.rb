28a8f4737b948630cb3fdb6f9449a0be0fba440c
Tom White <eugene.white@gmail.com>
2019-06-15 17:08:00 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,17 @@
 module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-   def reset_all
-     self.all.clear
-   end
+    def count
+      self.all.count
+    end
+  end
 
-   def count
-     self.all.count
-   end
-
+  module InstanceMethods
+    def initialize
+      # some more code coming soon!
+    end
+  end
 end
