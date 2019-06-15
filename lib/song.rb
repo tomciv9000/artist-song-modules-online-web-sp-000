e14221ae0df2d034492c7837a0a298e8888c91e6
Tom White <eugene.white@gmail.com>
2019-06-15 17:10:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,8 @@ require 'pry'
 
 class Song
 
-  extend Memorable
-  extend Findable
-  include Paramable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
 
   attr_accessor :name
   attr_reader :artist
